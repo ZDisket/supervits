420e3cc0722b4501b2a0efda8e763ae0f59f3119
ZDisket <30500847+ZDisket@users.noreply.github.com>
2025-12-31 20:56:27 -0300

    fix circular import

--- models.py
+++ models.py
@@ -195,7 +195,7 @@ class ResidualCouplingBlock(nn.Module):
 
     self.flows = nn.ModuleList()
     for i in range(n_flows):
-      self.flows.append(modules.ResidualCouplingLayer(channels, hidden_channels, kernel_size, dilation_rate, n_layers, gin_channels=gin_channels, mean_only=True, use_transformer=True))
+      self.flows.append(ResidualCouplingLayer(channels, hidden_channels, kernel_size, dilation_rate, n_layers, gin_channels=gin_channels, mean_only=True, use_transformer=True))
       self.flows.append(modules.Flip())
 
   def forward(self, x, x_mask, g=None, reverse=False):
@@ -206,6 +206,68 @@ class ResidualCouplingBlock(nn.Module):
       for flow in reversed(self.flows):
         x = flow(x, x_mask, g=g, reverse=reverse)
     return x
+
+class ResidualCouplingLayer(nn.Module):
+  def __init__(self,
+      channels,
+      hidden_channels,
+      kernel_size,
+      dilation_rate,
+      n_layers,
+      p_dropout=0,
+      gin_channels=0,
+      mean_only=False,
+      use_transformer=False):
+    assert channels % 2 == 0, "channels should be divisible by 2"
+    super().__init__()
+    self.channels = channels
+    self.hidden_channels = hidden_channels
+    self.kernel_size = kernel_size
+    self.dilation_rate = dilation_rate
+    self.n_layers = n_layers
+    self.half_channels = channels // 2
+    self.mean_only = mean_only
+    self.use_transformer = use_transformer
+
+    if self.use_transformer:
+      self.transformer = attentions.Encoder(self.half_channels, self.half_channels, 2, 1, 3, 0.1, 5.0)
+    else:
+      self.transformer = None
+
+    self.pre = nn.Conv1d(self.half_channels, hidden_channels, 1)
+    self.enc = modules.WN(hidden_channels, kernel_size, dilation_rate, n_layers, p_dropout=p_dropout, gin_channels=gin_channels)
+    self.post = nn.Conv1d(hidden_channels, self.half_channels * (2 - mean_only), 1)
+    self.post.weight.data.zero_()
+    self.post.bias.data.zero_()
+
+  def forward(self, x, x_mask, g=None, reverse=False):
+    x0, x1 = torch.split(x, [self.half_channels]*2, 1)
+
+    if self.use_transformer:
+      residual = x0 * x_mask
+      residual = self.transformer(residual, x_mask)
+      x0 = x0 + residual
+
+    h = self.pre(x0) * x_mask
+    h = self.enc(h, x_mask, g=g)
+    stats = self.post(h) * x_mask
+    if not self.mean_only:
+      m, logs = torch.split(stats, [self.half_channels]*2, 1)
+    else:
+      m = stats
+      logs = torch.zeros_like(m)
+
+    if not reverse:
+      x1 = m + x1 * torch.exp(logs) * x_mask
+      x = torch.cat([x0, x1], 1)
+      logdet = torch.sum(logs, [1,2])
+      return x, logdet
+    else:
+      x1 = (x1 - m) * torch.exp(-logs) * x_mask
+      x = torch.cat([x0, x1], 1)
+      return x
+
+
 
 
 class PosteriorEncoder(nn.Module):
